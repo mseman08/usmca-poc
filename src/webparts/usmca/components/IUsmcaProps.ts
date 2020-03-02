3ec051de33e033cb8341c3778fbae6a685a60994
Michael Seman <mseman@MILITSDEV.onmicrosoft.com>
2020-03-02 14:25:26 -0500

    initial commit

---
+++ src/webparts/usmca/components/IUsmcaProps.ts
@@ -0,0 +1,3 @@
+export interface IUsmcaProps {
+  description: string;
+}
---
+++ src/webparts/usmca/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IUsmcaWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'UsmcaWebPartStrings' {
+  const strings: IUsmcaWebPartStrings;
+  export = strings;
+}
